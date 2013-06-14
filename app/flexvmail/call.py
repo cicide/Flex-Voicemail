26d3f37eba9372c9540399166f2ef0c5e63ea94b
Chris Icide <chris@netgeeks.net>
2013-06-14 11:16:34 -0700

    I don't know why the code is stopping where it is

--- app/flexvmail/call.py
+++ app/flexvmail/call.py
@@ -159,12 +159,12 @@ class Call:
             resType = result['type']
             if resType == 'record':
                 log.debug('found a record result type')
-                vmFile = str(result['file_loc'])
-                log.debug('vmFile set to: %s' % vmFile)
+                #vmFile = str(result['file_loc'])
+                #log.debug('vmFile set to: %s' % vmFile)
                 act = str(nextAction)
                 log.debug('act set to: %s' % action)
                 log.debug('creating action request')
-                actionRequest = self.wsApiHost.wsapiCall(act, None, None, vmfile=vmFile)
+                actionRequest = self.wsApiHost.wsapiCall(act, None, None, vmfile=result['file_loc'])
                 log.debug('adding callbacks to action request')
                 actionRequest.addCallbacks(self.onActionResponse,self.onError)
                 log.debug('returning action request deferred')
