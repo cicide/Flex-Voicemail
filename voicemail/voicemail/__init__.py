e5cfe66cbb87ebecbf21deea9478ba4e4ba8d5e8
Abdul Wahid <abdulwahid24@gmail.com>
2013-07-29 10:55:20 +0530

    Implemented voice mail delete functionality.

--- voicemail/voicemail/__init__.py
+++ voicemail/voicemail/__init__.py
@@ -38,6 +38,7 @@ def main(global_config, **settings):
     config.add_route('view_vm', '/vm/view')
     config.add_route('play_vm', '/vm/play/{vmid}')
     config.add_route('download_vm', '/vm/download/{vmid}')
+    config.add_route('delete_vm', '/vm/delete/{vmid}')
     config.add_route('startcall', '/startcall')
     config.add_route('savemessage', '/savemessage')
     config.add_route('handlekey', '/handlekey')
