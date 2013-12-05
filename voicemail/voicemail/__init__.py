798a1fb714f2030b72f2dec795c509df82d2b4f7
riteshb <riteshb@gmail.com>
2013-12-05 00:11:43 -0800

    added email sending of vm

--- voicemail/voicemail/__init__.py
+++ voicemail/voicemail/__init__.py
@@ -11,6 +11,25 @@ from .models.models import (
     Base,
     )
 
+from sqlalchemy import exc
+from sqlalchemy import event
+from sqlalchemy.pool import Pool
+
+@event.listens_for(Pool, "checkout")
+def ping_connection(dbapi_connection, connection_record, connection_proxy):
+    cursor = dbapi_connection.cursor()
+    try:
+        cursor.execute("SELECT 1") 
+    except:
+        # optional - dispose the whole pool
+        # instead of invalidating one at a time
+        # connection_proxy._pool.dispose()
+        # raise DisconnectionError - pool will try 
+        # connecting again up to three times before raising.
+        raise exc.DisconnectionError()
+    cursor.close()
+
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
--- voicemail/voicemail/views/api.py
+++ voicemail/voicemail/views/api.py
@@ -39,6 +39,13 @@ from bag.web.pyramid.flash_msg import FlashMessage
 import json
 import requests
 
+import smtplib
+import os
+
+# Here are the email package modules we'll need
+from email.mime.audio import MIMEAudio
+from email.mime.multipart import MIMEMultipart
+COMMASPACE = ', '
 import logging
 log = logging.getLogger(__name__)
 
@@ -1341,17 +1348,34 @@ def deliverMessage(request, user, extension, cid_number, vmfile, duration, vm=No
     tuser = user
     if not tuser:
         tuser = DBSession.query(User).filter_by(extension=extension).first()
+    email_with_attachment = []
+    email_message = []
+    sms_message = []
+    subject = "New Voicemail"
+    time_message = datetime.datetime.utcnow()
+    preamble = 'Voicemail received from %s on %s duration %d seconds' % (cid_number, time_message, duration)
+    smtphost = request.registry.settings['smtp_host']
+    mail_from = request.registry.settings['mail_from']
+    file = vmfile[6:] # remove file:/
     if tuser and tuser.is_list:
         for j in tuser.members:
             vuser = DBSession.query(User).filter_by(extension=j).first() 
             v = Voicemail()
             v.cid_number = cid_number
             v.path = vmfile
-            v.create_date = datetime.datetime.utcnow()
+            v.create_date = time_message
             v.is_read = False
             v.status = 0
             v.duration = duration
             v.user = vuser
+            if vuser.vm_prefs.deliver_vm == 1:
+                if vuser.vm_prefs.email:
+                    if vuser.vm_prefs.attach_vm == 1:
+                        email_with_attachement.append(vuser.vm_prefs.email)
+                    else:
+                        email_message.append(vuser.vm_prefs.email)
+                if vuser_vm_prefs.sms_addr:
+                    sms_message.append(vuser.vm_prefs.email)
             if vm:
                 reply_to = ReplyTo()
                 reply_to.parentVoicemail = vm
@@ -1367,11 +1391,19 @@ def deliverMessage(request, user, extension, cid_number, vmfile, duration, vm=No
         v = Voicemail()
         v.cid_number = cid_number
         v.path = vmfile
-        v.create_date = datetime.datetime.utcnow()
+        v.create_date = time_message
         v.is_read = False
         v.status = 0
         v.duration = duration
         v.user = tuser
+        if tuser.vm_prefs.deliver_vm == 1:
+            if tuser.vm_prefs.email:
+                if tuser.vm_prefs.attach_vm == 1:
+                    email_with_attachement.append(tuser.vm_prefs.email)
+                else:
+                    email_message.append(tuser.vm_prefs.email)
+            if tuser_vm_prefs.sms_addr:
+                sms_message.append(tuser.vm_prefs.email)
         if vm:
             reply_to = ReplyTo()
             reply_to.parentVoicemail = vm
@@ -1383,6 +1415,12 @@ def deliverMessage(request, user, extension, cid_number, vmfile, duration, vm=No
         DBSession.flush()
         DBSession.refresh(tuser)
         postMWI(request, tuser)
+    if len(email_message) != 0:
+        sendEmail(request, subject, mail_from, email_message, None, preamble, smtphost)
+    if len(email_with_attachment) != 0:
+        sendEmail(request, subject, mail_from, email_with_attachment, file, preamble, smtphost)
+    if len(sms_message) != 0:
+        sendEmail(request, subject, mail_from, sms_message, None, preamble, smtphost)
 
 
 
@@ -1434,3 +1472,28 @@ def returnLoggedIn(request, user, callid, promptBefore):
         nextaction=state.nextaction,
         dtmf=state.dtmf
         )
+
+def sendEmail(request, subject, mail_from, list_of_recipients, file, preamble, smtphost):
+    # Create the container (outer) email message.
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    me = mail_from
+    # family = the list of all recipients' email addresses
+    family = list_of_recipients
+    msg['From'] = me
+    msg['BCC'] = COMMASPACE.join(family)
+    msg.preamble = preamble
+    if file:
+        # Open the files in binary mode.  Let the MIMEAudio class automatically
+        # guess the specific audio type.
+        fp = open(file, 'rb')
+        wav = MIMEAudio(fp.read())
+        fp.close()
+        wav.add_header('Content-Disposition', 'attachment', filename=os.path.basename(file))
+        msg.attach(wav)
+
+    # Send the email via our own SMTP server.
+    s = smtplib.SMTP(smtphost)
+    s.sendmail(me, family, msg.as_string())
+    s.quit()
+
